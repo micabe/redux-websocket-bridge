c10ba920e24aa412bb3b6b190e7a8ad2eaa22c6f
mabensur <mabensur@qynapse.com>
2020-02-05 19:13:25 +0100

    Use socket io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import blobToArrayBuffer from './blobToArrayBuffer';
-import isFSA             from './isFSA';
+import io from 'socket.io-client'
+import blobToArrayBuffer from './blobToArrayBuffer'
+import isFSA from './isFSA'
 
-export const CLOSE   = `CLOSE`;
-export const MESSAGE = `MESSAGE`;
-export const OPEN    = `OPEN`;
-export const SEND    = `SEND`;
+export const CLOSE = `CLOSE`
+export const MESSAGE = `MESSAGE`
+export const OPEN = `OPEN`
+export const SEND = `SEND`
 
 export function close() {
-  return { type: CLOSE };
+  return { type: CLOSE }
 }
 
 export function message(payload) {
@@ -15,116 +16,142 @@ export function message(payload) {
 }
 
 export function open() {
-  return { type: OPEN };
+  return { type: OPEN }
 }
 
 const DEFAULT_OPTIONS = {
   binaryType: 'arraybuffer',
-  fold      : (action, webSocket) => {
-    if (action.meta && arrayify(action.meta.send).some(send => send === true || send === webSocket)) {
-      const { meta, ...actionWithoutMeta } = action;
+  fold: (action, webSocket) => {
+    if (
+      action.meta &&
+      arrayify(action.meta.send).some(send => send === true || send === webSocket)
+    ) {
+      const { meta, ...actionWithoutMeta } = action
 
-      return JSON.stringify(actionWithoutMeta);
+      return JSON.stringify(actionWithoutMeta)
     }
   },
-  meta      : {},
-  namespace : '@@websocket/',
-  unfold   : (payload, webSocket, raw) => {
-    const action = tryParseJSON(payload);
+  meta: {},
+  namespace: '@@websocket/',
+  unfold: (payload, webSocket, raw) => {
+    const action = tryParseJSON(payload)
 
-    return action && {
-      ...action,
-      meta: {
-        ...action.meta,
-        webSocket
+    return (
+      action && {
+        ...action,
+        meta: {
+          ...action.meta,
+          webSocket,
+        },
       }
-    };
-  }
-};
+    )
+  },
+}
 
 function arrayify(obj) {
-  return obj ? Array.isArray(obj) ? obj : [obj] : [];
+  return obj ? (Array.isArray(obj) ? obj : [obj]) : []
 }
 
 export default function createWebSocketMiddleware(urlOrFactory, options = DEFAULT_OPTIONS) {
-  options = { ...DEFAULT_OPTIONS, ...options };
-  options.binaryType = options.binaryType.toLowerCase();
-  options.unfold = options.unfold && (typeof options.unfold === 'function' ? options.unfold : DEFAULT_OPTIONS.unfold);
+  options = { ...DEFAULT_OPTIONS, ...options }
+  options.binaryType = options.binaryType.toLowerCase()
+  options.unfold =
+    options.unfold &&
+    (typeof options.unfold === 'function' ? options.unfold : DEFAULT_OPTIONS.unfold)
 
-  const { namespace } = options;
+  const { namespace } = options
 
   return store => {
-    let webSocket;
+    const socket = io(urlOrFactory, {
+      transports: ['websocket'],
+      upgrade: true,
+    })
 
-    if (typeof urlOrFactory === 'function') {
-      webSocket = urlOrFactory();
-    } else {
-      webSocket = new WebSocket(urlOrFactory);
-    }
+    const patch = require('socketio-wildcard')(io.Manager)
+    patch(socket)
 
-    webSocket.onopen = () => store.dispatch({ type: `${ namespace }${ OPEN }`, meta: { webSocket } });
-    webSocket.onclose = () => store.dispatch({ type: `${ namespace }${ CLOSE }`, meta: { webSocket } });
-    webSocket.onmessage = event => {
-      let getPayload;
+    const webSocket = socket
+    socket.on('connect', () => {
+      store.dispatch({ type: `${namespace}${OPEN}`, meta: { webSocket } })
+    })
+    socket.on('disconnect', () => {
+      store.dispatch({ type: `${namespace}${CLOSE}`, meta: { webSocket } })
+    })
 
-      if (typeof Blob !== 'undefined' && options.binaryType === 'arraybuffer' && event.data instanceof Blob) {
-        getPayload = blobToArrayBuffer(event.data);
-      } else if (typeof ArrayBuffer !== 'undefined' && options.binaryType === 'blob' && event.data instanceof ArrayBuffer) {
-        getPayload = new Blob([event.data]);
+    // on reconnection, reset the transports option, as the Websocket
+    // connection may have failed (caused by proxy, firewall, browser, ...)
+    // socket.on('reconnect_attempt', () => {
+    //   socket.io.opts.transports = ['polling', 'websocket']
+    // })
+
+    socket.on('*', event => {
+      console.log(event)
+      if (
+        typeof Blob !== 'undefined' &&
+        options.binaryType === 'arraybuffer' &&
+        event.data instanceof Blob
+      ) {
+        getPayload = blobToArrayBuffer(event.data)
+      } else if (
+        typeof ArrayBuffer !== 'undefined' &&
+        options.binaryType === 'blob' &&
+        event.data instanceof ArrayBuffer
+      ) {
+        getPayload = new Blob([event.data])
       } else {
         // We make this a Promise because we might want to keep the sequence of dispatch, @@websocket/MESSAGE first, then unfold later.
-        getPayload = Promise.resolve(event.data);
+        getPayload = Promise.resolve(event.data)
       }
 
       return getPayload.then(payload => {
         if (options.unfold) {
-          const action = options.unfold(payload, webSocket, payload);
+          const action = options.unfold(payload, webSocket, payload)
 
           if (action) {
             if (!isFSA(action)) {
-              throw new Error('Unfolded action is not a Flux Standard Action compliant');
+              throw new Error('Unfolded action is not a Flux Standard Action compliant')
             }
 
-            return action && store.dispatch(action);
+            return action && store.dispatch(action)
           }
         }
 
         store.dispatch({
-          type: `${ namespace }${ MESSAGE }`,
+          type: `${namespace}${MESSAGE}`,
           meta: { webSocket },
-          payload
-        });
-      });
-    }
+          payload,
+        })
+      })
+    })
 
     return next => action => {
-      if (action.type === `${ namespace }${ SEND }`) {
-        webSocket.send(action.payload);
+      if (action.type === `${namespace}${SEND}`) {
+        socket.send(action.payload)
       } else {
-        const payload = options.fold(action, webSocket);
+        const payload = options.fold(action, webSocket)
 
-        payload && webSocket.send(payload);
+        payload && socket.send(payload)
       }
 
-      return next(action);
-    };
-  };
+      return next(action)
+    }
+  }
 }
 
 function tryParseJSON(json) {
   try {
-    return JSON.parse(json);
+    return JSON.parse(json)
   } catch (err) {}
 }
 
 export function trimUndefined(map) {
   return Object.keys(map).reduce((nextMap, key) => {
-    const value = map[key];
+    const value = map[key]
 
     if (typeof value !== 'undefined') {
-      nextMap[key] = value;
+      nextMap[key] = value
     }
 
-    return nextMap;
-  }, {});
+    return nextMap
+  }, {})
 }
